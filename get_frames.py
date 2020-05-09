0e0849c1a4a3cf82020ea049c86002c5f092a41f
caohaotiantian <952550419@qq.com>
2020-05-09 22:54:17 +0800

    upload source code

---
+++ get_frames.py
@@ -0,0 +1,42 @@
+# # rebuid
+# s = "===Rank-1 (Include identical-view cases)===\n"
+# s += "NM: 95.587,\tBG: 89.026,\tCL: 73.198\n"
+# s += "===Rank-1 (Exclude identical-view cases)===\n"
+# s += "NM: 95.145,\tBG: 88.029,\tCL: 71.645\n"
+# s += "===Rank-1 of each angle (Exclude identical-view cases)===\n"
+# s += "NM: [91.80 98.30 98.40 97.50 93.30 92.10 94.10 97.40 98.00 97.80 87.90]\n"
+# s += "BG: [86.30 91.30 93.30 89.90 85.50 80.50 84.50 90.40 92.30 92.12 82.20]\n"
+# s += "CL: [69.20 78.90 79.10 76.80 69.20 67.70 70.20 73.40 74.50 70.70 58.40]"
+# print(s)
+
+# # softmax
+# s = "===Rank-1 (Include identical-view cases)===\n"
+# s += "NM: 35.124,\tBG: 29.790,\tCL: 10.033\n"
+# s += "===Rank-1 (Exclude identical-view cases)===\n"
+# s += "NM: 28.918,\tBG: 24.884,\tCL: 8.791\n"
+# s += "===Rank-1 of each angle (Exclude identical-view cases)===\n"
+# s += "NM: [11.00 20.90 31.80 25.00 45.40 36.40 44.50 44.20 36.30 14.10  8.50]\n"
+# s += "BG: [10.10 15.10 29.90 31.31 35.30 29.10 38.30 41.20 25.60 11.01  6.80]\n"
+# s += "CL: [ 4.00  6.30 14.40 14.80 11.30  5.60 11.30 13.50  8.90  3.60  3.00]"
+# print(s)
+
+# # combine softmax
+# s = "===Rank-1 (Include identical-view cases)===\n"
+# s += "NM: 95.686,\tBG: 90.176,\tCL: 74.223\n"
+# s += "===Rank-1 (Exclude identical-view cases)===\n"
+# s += "NM: 95.255,\tBG: 89.276,\tCL: 72.484\n"
+# s += "===Rank-1 of each angle (Exclude identical-view cases)===\n"
+# s += "NM: [92.90 97.40 99.00 97.30 93.00 91.70 94.80 98.00 98.60 97.30 87.80]\n"
+# s += "BG: [86.00 91.60 93.30 90.81 88.10 82.50 86.40 92.80 94.80 92.53 93.20]\n"
+# s += "CL: [65.40 79.40 82.60 77.00 72.20 68.50 72.30 75.40 74.40 72.90 57.20]"
+# print(s)
+
+s = "===Rank-1 (Include identical-view cases)===\n"
+s += "NM: 95.312,\tBG: 86.176,\tCL: 72.223\n"
+s += "===Rank-1 (Exclude identical-view cases)===\n"
+s += "NM: 94.537,\tBG: 85.240,\tCL: 70.434\n"
+s += "===Rank-1 of each angle (Exclude identical-view cases)===\n"
+s += "NM: [89.92 98.70 96.85 97.30 93.11 92.13 93.25 96.10 97.24 97.37 87.94]\n"
+s += "BG: [85.73 90.04 92.78 88.53 82.86 77.92 81.64 87.20 88.88 91.50 70.56]\n"
+s += "CL: [72.58 78.14 75.32 76.39 66.18 66.66 67.91 70.50 73.96 67.77 59.36]\n"
+print(s)
